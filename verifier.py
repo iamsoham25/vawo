c86e398fc32699e0f9ef1034b47bab4ff2ec674d
iamsoham25 <sohamthoke@gmail.com>
2026-09-03 11:14:14 +0530

    updating verify.py

--- verifier.py
+++ verifier.py
@@ -10,3 +10,14 @@ from signing import (
     verify_signature
 )
 
+class VerificationResult(BaseModel):
+    """
+    Stores the result of a VAWO verification process.
+    """
+
+    accepted: bool
+    reason: str
+    checks_passed: list[str]
+    checks_failed: list[str]
+
+
