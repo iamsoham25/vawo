477270ee4a47691cf41f58f5d49de8ec8c1cab63
iamsoham25 <sohamthoke@gmail.com>
2026-09-09 11:06:56 +0530

    worker_agent_cheat.py

---
+++ worker_agent_cheat.py
@@ -0,0 +1,22 @@
+import asyncio
+import json
+import os
+import uvicorn
+
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.server.routes import (
+    create_agent_card_routes,
+    create_jsonrpc_routes,
+)
+from a2a.types import (
+    AgentCard,
+    AgentCapabilities,
+    AgentInterface,
+    AgentSkill,
+)
+from a2a.utils import (
+    get_data_parts,
+    new_data_part,
+)
